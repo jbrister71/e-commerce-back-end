a3679a2328c003aab91c34971ac3b9bbe4ce17d1
Bell <jacobbrister71@gmail.com>
2022-09-08 16:15:41 -0400

    updated README

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -83,7 +83,7 @@ router.post('/', (req, res) => {
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      console.log(err);
+      // console.log(err);
       res.status(400).json(err);
     });
 });
@@ -125,7 +125,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+       console.log(err);
       res.status(400).json(err);
     });
 });
