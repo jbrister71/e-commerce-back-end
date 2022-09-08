f637973dd6ccae5d38dd6d77155538274c68e866
Bell <jacobbrister71@gmail.com>
2022-09-08 01:02:38 -0400

    added put route for tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -67,17 +67,16 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
-    individualHooks: true,
     where: {
       id: req.params.id
     }
   })
-    .then(dbTagData => {
-      if(!dbTagData[0]) {
-        res.status(404).json({ message: 'No tag found with this id' });
+    .then(dbCategoryData => {
+      if(!dbCategoryData[0]) {
+        res.status(404).json({ message: 'No category found with this id' });
         return;
       }
-      res.json(dbTagData);
+      res.json(dbCategoryData);
     })
     .catch(err => {
       console.log(err);
